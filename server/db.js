4b778f71c59c189cf63f1a60aeb3530dff1c5d13
Evan Spiler <monkeysRawesome@gmail.com>
2014-12-11 13:40:11 -0800

    prompt>game

--- server/db.js
+++ server/db.js
@@ -24,6 +24,7 @@ db.schema.hasTable('users').then(function (exists) {
       user.increments('id').primary();
       user.string('email', 255);
       user.string('password', 255);
+      user.string('photoFilename', 255); //Relative to /media/
       user.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
@@ -31,18 +32,30 @@ db.schema.hasTable('users').then(function (exists) {
   }
 });
 
-// Model for prompts - one to many photo, winner - photo id, start time and end time, voting end time, title
-db.schema.hasTable('prompts').then(function (exists) {
+//friends user_user join
+db.schema.hasTable('user_user-join').then(function (exists) {
   if (!exists) {
-    db.schema.createTable('prompts', function (prompt) {
-      prompt.increments('id').primary();
-      prompt.string('title', 255);
-      prompt.integer('winner_id').references('photos.id');
-      prompt.integer('user_id').references('users.id');
-      prompt.timestamp('startTime'); // TODO: Change to lower_case
-      prompt.timestamp('endTime'); // TODO: Change to lower_case
-      prompt.timestamp('votingEndTime'); // TODO: Change to lower_case
-      prompt.timestamps();
+    db.schema.createTable('user_user-join', function (friends) {
+      friends.integer('friend1').references('users.id');
+      friends.integer('friend2').references('users.id');
+      friends.boolean('accepted')
+    })
+  }
+})
+
+// Model for games - one to many photo, winner - photo id, start time and end time, voting end time, title
+db.schema.hasTable('games').then(function (exists) {
+  if (!exists) {
+    db.schema.createTable('games', function (game) {
+      game.increments('id').primary();
+      game.string('prompt', 255);
+      game.integer('winPhoto_id').references('photos.id');
+      game.integer('winPlayer_id').references('users.id');
+      game.integer('owner_id').references('users.id');
+      game.timestamp('startTime'); // TODO: Change to lower_case
+      game.timestamp('endTime'); // TODO: Change to lower_case
+      game.timestamp('votingEndTime'); // TODO: Change to lower_case
+      game.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
     });
@@ -56,7 +69,7 @@ db.schema.hasTable('photos').then(function (exists) {
       photo.increments('id').primary();
       photo.string('filename', 255); // Relative to /media/
       photo.integer('user_id').references('users.id');
-      photo.integer('prompt_id').references('prompts.id');
+      photo.integer('game_id').references('games.id');
       photo.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
@@ -64,14 +77,29 @@ db.schema.hasTable('photos').then(function (exists) {
   }
 });
 
-// Comment - one to one with user, belongsTo prompts - needs a user and needs a photo
+// User_Game_Photo-Join
+db.schema.hasTable('user_game_photo-join').then(function (exists) {
+  if (!exists) {
+    db.schema.createTable('user_game_photo-join', function (ugpj) {
+      ugpj.integer('user_id').references('users.id');
+      ugpj.integer('game_id').references('games.id');
+      ugpj.integer('photo_id').references('photos.id');
+      ugpj.boolean('joined');
+      ugpj.boolean('submitted');
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+// Comment - one to one with user, belongsTo games - needs a user and needs a photo
 db.schema.hasTable('comments').then(function (exists) {
   if (!exists) {
     db.schema.createTable('comments', function (photo) {
       photo.increments('id').primary();
       photo.string('content', 255).notNullable();
       photo.integer('user_id').references('users.id').notNullable();
-      photo.integer('prompt_id').references('prompts.id').notNullable();
+      photo.integer('prompt_id').references('games.id').notNullable();
       photo.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
