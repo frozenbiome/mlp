dc64e36ef3302d38919430484c762ca56e3db305
Alex Hawkins <alexhawkins.me@gmail.com>
2014-12-12 11:20:33 -0800

    implement image uploading from disk

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,34 +5,34 @@ angular.module('voto.controllers', [])
   //Auth.isAuth();
   //$scope.signOut = Auth.signOut;
   //$scope.limitChar = function (string, limit) {
-    //return (string.length < limit) ? string : string.substr(0, limit) + "...";
+  //return (string.length < limit) ? string : string.substr(0, limit) + "...";
   //};
   //
 
-  $scope.saveGameToRootScope = function (game) {
+  $scope.saveGameToRootScope = function(game) {
     console.log("Saving game to scope: ", game)
     $rootScope.currentGame = game;
   }
 
   //Gets all games user created
-  $scope.getAllGamesForUser = function () {
+  $scope.getAllGamesForUser = function() {
     if ($rootScope.user) {
       GamesFactory.getAllGamesForUser($rootScope.user)
-      .then(function(res) {
-        $scope.createdGames = res.data;
-        console.log("Games User Created: ", res.data);
-      });
+        .then(function(res) {
+          $scope.createdGames = res.data;
+          console.log("Games User Created: ", res.data);
+        });
     }
   }
 
   //Gets all games user submitted to
-  $scope.getAllPlayingGames = function () {
+  $scope.getAllPlayingGames = function() {
     if ($rootScope.user) {
       GamesFactory.getAllPlayingGames($rootScope.user)
-      .then(function(res) {
-        $scope.playingGames = res.data;
-        console.log("Games User Submitted to: ", res.data);
-      });
+        .then(function(res) {
+          $scope.playingGames = res.data;
+          console.log("Games User Submitted to: ", res.data);
+        });
     }
   }
 
@@ -45,22 +45,19 @@ angular.module('voto.controllers', [])
         title: 'Pick a Name',
         subTitle: 'any name...',
         scope: $scope,
-        buttons: [
-          {
-            text: '<b>Save</b>',
-            type: 'button-positive',
-            onTap: function(e) {
-            }
-          },
-        ]
+        buttons: [{
+          text: '<b>Save</b>',
+          type: 'button-positive',
+          onTap: function(e) {}
+        }, ]
       });
       myPopup.then(function(res) {
         GamesFactory.getUserInfo($scope.data.username)
-        .then(function(user) {
-          $rootScope.user = user;
-          $scope.getAllGamesForUser();
-          $scope.getAllPlayingGames();
-        })
+          .then(function(user) {
+            $rootScope.user = user;
+            $scope.getAllGamesForUser();
+            $scope.getAllPlayingGames();
+          })
       });
     }
   }();
@@ -70,14 +67,13 @@ angular.module('voto.controllers', [])
 .controller('CreateCtrl', function($scope, $rootScope, Create) {
   $scope.games = Create.all();
 
-  $scope.getAllPublicGames = function () {
+  $scope.getAllPublicGames = function() {
 
     Create.getAllPublicGames($rootScope.user)
-    .then(function(res) {
-      console.log(res)
-      $scope.publicGames = res.data;
-      console.log("Public Games: ", res.data);
-    });
+      .then(function(res) {
+        $scope.publicGames = res.data;
+        console.log("Public Games: ", res.data);
+      });
   }
 
 })
@@ -97,29 +93,48 @@ angular.module('voto.controllers', [])
 
 })
 
-.controller('GameCtrl', function($scope, $rootScope, $http, GamesFactory, $cordovaCamera) {
-   $scope.takePicture = function() {
-    var options = { 
-        quality : 75, 
-        destinationType : Camera.DestinationType.DATA_URL, 
-        sourceType : Camera.PictureSourceType.CAMERA, 
-        allowEdit : true,
-        encodingType: Camera.EncodingType.JPEG,
-        targetWidth: 300,
-        targetHeight: 300,
-        popoverOptions: CameraPopoverOptions,
-        saveToPhotoAlbum: true
+.controller('GameCtrl', function($scope, $rootScope, $http, GamesFactory, $cordovaCamera, $ionicLoading) {
+  $scope.takePicture = function() {
+    var options = {
+      quality: 75,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 300,
+      targetHeight: 300,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: true
     };
 
     $cordovaCamera.getPicture(options)
       .then(function(imageData) {
         //Changed to png
-          $scope.imgURI = "data:image/png;base64," + imageData;
+        $scope.imgURI = "data:image/png;base64," + imageData;
       })
       .catch(function(err) {
         // An error occured. Show a message to the user
       });
   }
+
+  $scope.selectPicture = function() {
+    var options = {
+      targetWidth: 300,
+      targetHeight: 300,
+      quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+    };
+
+    $cordovaCamera.getPicture(options)
+      .then(function(imageData) {
+        //Changed to png
+        $scope.imgURI = "data:image/png;base64," + imageData;
+      })
+      .catch(function(err) {
+        // An error occured. Show a message to the user
+      });
+  };
 
   $scope.submitPhoto = function() {
     alert($scope.imgURI);
@@ -127,37 +142,37 @@ angular.module('voto.controllers', [])
 
     var curr_game = $rootScope.currentGame;
 
-    $http({ 
-      url: "http://10.8.16.232:8000/api/photo",
-      method: "POST", 
-      params: {
-        prompt_id: curr_game.id,
-        user_id: $rootScope.user,
-        //prompt_id: 1,
-        //user_id: 2,
-        image_string: $scope.imgURI
-      }
-    })
-    .success(function(data, status, headers, config) {
-      console.log("DATA", data);
-    })
-    .error(function(error) {
-      console.log("ERROR", error);
-    })
-/*    $scope.upload = $upload.upload({*/
-        //url: '/api/photo',
-        //method: 'POST',
-        //data: {
-          //prompt_id: $scope.id,
-          //user_id: Auth.getUserId(),
-          //image_data: image.src
-        //},
+    $http({
+        url: "http://10.8.16.232:8000/api/photo",
+        method: "POST",
+        params: {
+          prompt_id: curr_game.id,
+          user_id: $rootScope.user,
+          //prompt_id: 1,
+          //user_id: 2,
+          image_string: $scope.imgURI
+        }
+      })
+      .success(function(data, status, headers, config) {
+        console.log("DATA", data);
+      })
+      .error(function(error) {
+        console.log("ERROR", error);
+      })
+      /*    $scope.upload = $upload.upload({*/
+      //url: '/api/photo',
+      //method: 'POST',
+      //data: {
+      //prompt_id: $scope.id,
+      //user_id: Auth.getUserId(),
+      //image_data: image.src
+      //},
       //})
-    //.success(function (data, status, headers, config) {
-        //console.log(data);
-        //$state.reload();
+      //.success(function (data, status, headers, config) {
+      //console.log(data);
+      //$state.reload();
       //}).error(function (err) {
-        //console.log('ERROR:', err);
+      //console.log('ERROR:', err);
       //})
 
   };
@@ -173,14 +188,14 @@ angular.module('voto.controllers', [])
 
   //Maybe not necessary, as getGameData should take care of this
   //$scope.getAllPhotosForGame = function() {
-    //console.log("GetAllPhotos")
-    //$scope.photos = [];
-    //GamesFactory.getAllPhotosForGame($rootScope.currentGame.id)
-    //.then(function(photos) {
-      //photos.forEach(function(photo) {
-        //$scope.photos.push(photo);
-      //})
-    //})
+  //console.log("GetAllPhotos")
+  //$scope.photos = [];
+  //GamesFactory.getAllPhotosForGame($rootScope.currentGame.id)
+  //.then(function(photos) {
+  //photos.forEach(function(photo) {
+  //$scope.photos.push(photo);
+  //})
+  //})
   //};
 
   $scope.selectPhoto = function(photo) {
@@ -196,5 +211,3 @@ angular.module('voto.controllers', [])
 
 
 });
-
-
